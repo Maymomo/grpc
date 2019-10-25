9fa0c3bd4a873016d612b8096dcdef432cccecf7
Maymomo <3466365807@qq.com>
2019-10-25 05:10:24 +0000

    remove build

--- build/third_party/benchmark/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/grpc/third_party/benchmark
-# Build directory: /root/grpc/build/third_party/benchmark
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
